f27307ad5c5a9a5cc434dcd99c8ff38f15c562b2
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-07-21 01:02:05 +0530

    Added PageNotFound.jsx

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PageNotFound = () => {
+  return (
+    <div className="w-full h-screen bg-[#111] p-32 select-none font-black text-white flex flex-col items-center justify-start gap-4">
+      <Link
+        className="text-3xl absolute top-8 left-10 hover:text-gray-500 transition duration-300 cursor-pointer"
+        to="/"
+      >
+        ➥
+      </Link>
+      <h1 className="text-7xl font-bold">404</h1>
+      <h1 className="text-4xl font-bold">Page Not Found</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
