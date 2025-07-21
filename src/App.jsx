a4117a8331f6411ea48709914b40e78e9d9edc8c
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-07-21 23:26:09 +0530

    Implemented lenis for smooth scrolling to the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Mainroutes from "./routes/Mainroutes";
+import Lenis from "lenis";
 
 const App = () => {
+  const lenis = new Lenis();
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+
   return (
     <>
       <Mainroutes />
